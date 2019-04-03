0790be58b754ca9cc603fc1eb17f8637bdfc5cc6
wathigo <wathigosimon@gmail.com>
2019-04-03 04:08:48 -0400

    App configured for various environments

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+from instance.config import config_by_name
+
+db = SQLAlchemy()
+flask_bcrypt = Bcrypt()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    print(config_name)
+    app.config.from_object(config_by_name[config_name])
+    db.init_app(app)
+    flask_bcrypt.init_app(app)
+
+    return app
+
+from app.utils.database.schemas import Person, Area, Ward, Constituency, County
